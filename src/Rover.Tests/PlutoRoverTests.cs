0529abc63b97860c327d263006a1d75400b08d48
Ivan Blazhko <ivan.blazhko@gmail.com>
2018-09-01 13:11:10 +0100

    (Red) Move forward

--- src/Rover.Tests/PlutoRoverTests.cs
+++ src/Rover.Tests/PlutoRoverTests.cs
@@ -18,5 +18,20 @@ namespace Rover.Tests
             // Assert
             exception.Should().BeNull();
         }
+
+        [Fact]
+        public void When_MoveForwardOneStep_Expect_PositionChanged()
+        {
+            // Arrange
+            var pluto = new Pluto(100, 100);
+            var rover = new PlutoRover(pluto, new Position("0,0,N"));
+
+            // Act
+            rover.Move("F");
+
+            // Assert
+            rover.Position.ToString()
+                .Should().Be("0,1,N");
+        }
     }
 }
